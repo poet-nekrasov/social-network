f62152f2808d0adb5abc31545ded4cf7d5e1d9e6
poet-nekrasov <poet-nekrasov@mail.ru>
2023-08-12 23:43:57 +0500

    Add useEffect in UserStatus

--- src/components/ui/ProfileContainer/Profile/Info/UserStatus/UserStatus.jsx
+++ src/components/ui/ProfileContainer/Profile/Info/UserStatus/UserStatus.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const UserStatus = (props) => {
     let [status, setStatus] = useState(props.status);
 
     let [editMode, setEditMode] = useState(false);
 
-    const handleActivateEditMode = () => setEditMode(true);
+    useEffect(() => setStatus(props.status), [props.status]);
 
-    const handleChangeStatus = (e) => setStatus(e.target.value);
+    const handleActivateEditMode = () => setEditMode(true);
 
     const handleDeactivateEditMode = () => {
         setEditMode(false);
         props.updateProfileStatus(status);
     };
+
+    const handleChangeStatus = (e) => setStatus(e.target.value);
 
     return (
         <div>
@@ -35,63 +37,3 @@ export const UserStatus = (props) => {
         </div>
     );
 };
-// class UserStatus extends React.Component {
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.status !== this.props.status) {
-//       this.setState({
-//         status: this.props.status,
-//       });
-//     }
-//   }
-
-//   state = {
-//     editMode: false,
-//     status: this.props.status,
-//   };
-
-//   activateEditMode() {
-//     this.setState({
-//       editMode: true,
-//     });
-//   }
-
-//   deactivateEditMode() {
-//     this.setState({
-//       editMode: false,
-//     });
-
-//     this.props.updateProfileStatus(this.state.status);
-//   }
-
-//   handleChangeStatus(event) {
-//     this.setState({
-//       status: event.target.value,
-//     });
-//   }
-
-//   render() {
-//     console.log("render");
-//     return (
-//       <div>
-//         {this.state.editMode ? (
-//           <div>
-//             <input
-//               onBlur={this.deactivateEditMode.bind(this)}
-//               onChange={this.handleChangeStatus.bind(this)}
-//               type="text"
-//               autoFocus
-//               placeholder="Enter text is here"
-//               value={this.state.status}
-//             />
-
-//             <button>Set status</button>
-//           </div>
-//         ) : (
-//           <span onDoubleClick={this.activateEditMode.bind(this)}>
-//             {this.state.status}
-//           </span>
-//         )}
-//       </div>
-//     );
-//   }
-// }
